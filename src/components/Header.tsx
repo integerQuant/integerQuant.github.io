b6f9cedb72f7628bcc26a8c92b061880c836c55c
internQuant <cinzeis-rehang.0l@icloud.com>
2025-10-24 14:30:00 -0300

    tweak responsive CSS rules

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ export default function Header() {
   return (
     <header className="site container site-header">
       <div className="brand">RMTB | full stack quant developer | São Paulo - Brazil</div>
-      <a href="mailto:rodrigomtorresb@gmail.com">
+      <a className="contact-btn" href="mailto:rodrigomtorresb@gmail.com">
         get in touch
       </a>
     </header>
